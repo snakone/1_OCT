bf9f6938b5986e76fb86af47f88551313d7e0c3a
Sergi <snak_one@hotmail.com>
2018-09-28 12:34:55 +0200

    Images and CSS

--- 1oct/js/main.js
+++ 1oct/js/main.js
@@ -1,4 +1,7 @@
 var game;
+var gameFrame = document.getElementById("Canvas");
+
+
 window.onload = function()
 {
 	 isMobile=navigator.userAgent.indexOf("Mobile");
@@ -6,10 +9,11 @@ window.onload = function()
    if (isMobile==-1)
     {
         game=new Phaser.Game(640,480,Phaser.AUTO,"ph_game");
+
     }
     else
     {       
-      game=new Phaser.Game(window.innerWidth,window.innerHeight,Phaser.AUTO,"ph_game");  
+	  game=new Phaser.Game(window.innerWidth,window.innerHeight,Phaser.AUTO,"ph_game");  
       console.log("Mobile");
     }
 
